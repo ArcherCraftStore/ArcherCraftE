4fd559cd40d3c60b628ddcece776b39d551e537b
WH <weldon23.henson23@gmail.com>
2014-05-19 14:55:54 -0400

    new

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ var propeller = new Object();
 propeller.canvas=document.getElementById("canvas");
 if(propeller.canvas.getContext){
 	propeller.context=propeller.canvas.getContext("2d");
-}else{
-	alert("A0-0007: Canvas Unsupported");
+	 var img = new Image();   // Create new img element
+  img.src = 'warrior.png'; // Set source path
+  var warrior = {
+    x: 0,
+    y: 0
+  };
+  img.onload = function(){
+    propeller.context.drawImage(img,x,y);
+  };
+} else {
+ alert("A0-0007: Canvas Unsupported");
 }
+
