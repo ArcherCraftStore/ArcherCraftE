8c807c115bbaa2129d2390ea7a0aba9bd1a4173c
WH <weldon23.henson23@gmail.com>
2014-05-19 14:45:35 -0400

    New

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 var propeller = new Object();
+propeller.canvas=document.getElementById("canvas");
+if(propeller.canvas.getContext){
+	propeller.context=propeller.canvas.getContext("2d");
+}else{
+	alert("A0-0007: Canvas Unsupported");
+}
